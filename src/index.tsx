763a6ab9e3fb918ee4b18482e1056e19134c11f5
Vlased <vladyslav.boriatynskyi.pz.2021@lpnu.ua>
2022-10-07 22:59:47 +0300

    gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createGlobalStyle } from 'styled-components';
 import App from './App';
-import {BrowserRouter as Router} from "react-router-dom";
+import {HashRouter as Router} from "react-router-dom";
 import { RecoilRoot } from 'recoil';
 
 const Global = createGlobalStyle`
